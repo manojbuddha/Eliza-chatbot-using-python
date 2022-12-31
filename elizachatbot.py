93426486ac1b14f1fce1de24810cdc33f20546e5
Manoj Buddha <manojnaidu001@gmail.com>
2022-12-31 02:42:04 -0500

    Bug fix

--- elizachatbot.py
+++ elizachatbot.py
@@ -5,30 +5,22 @@ from string import punctuation
 import json
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import WordNetLemmatizer
 
 nltk.download('averaged_perceptron_tagger', quiet=True)
+
+# Defining required global variables
+global RANK
+global KEY_STACK
+global REPLIES
 
 
 # This function is used to load the pre-defined rules Eliza uses to spot keywords along with decomposition
 # rules(regular expression patterns) and rearrangement rules (replies).
 def load_dict():
-    with open("chatbot_rules.json", "r") as chatbot_rules_file:
-        chatbot_rules = json.load(chatbot_rules_file)
+    with open("chatbot_rules.json", "r") as chatbot_rules_json:
+        rules_dict = json.load(chatbot_rules_json)
+    return rules_dict
 
-    return chatbot_rules
-
-
-# Defining required global variables
-global RANK
-global key_stack
-global REPLIES
-global REPLY_LENGTH
-
-REPLY_LENGTH = 0
-REPLIES = []
-RANK = 0
-key_stack = []
 
 # Loading our pre-defined keyword dictionary
 null = 'null'
@@ -46,17 +38,17 @@ def clean_text(text):
 # arrange the keyword in a list. If the rank is higher, the keyword is palced in the begining of the list.
 def get_rank(token):
     global RANK
-    global key_stack
+    global KEY_STACK
+
     if chatbot_rules.get(token) is not None:
-        rank = chatbot_rules.get(token).get('RANK')
-        if rank is None:
-            rank = 0
-        if RANK > int(rank):
-            key_stack = key_stack + [token]
+        rank_ = chatbot_rules.get(token).get('RANK')
+        if rank_ is None:
+            rank_ = 0
+        if RANK > int(rank_):
+            KEY_STACK = KEY_STACK + [token]
         else:
-            key_stack = [token] + key_stack
-            RANK = int(rank)
-        return "TOKEN_FOUND"
+            KEY_STACK = [token] + KEY_STACK
+            RANK = int(rank_)
     else:
         return "TOKEN_NOT_FOUND"
 
@@ -65,8 +57,7 @@ def get_rank(token):
 # then apply decomposition and rearrangement rules for the keyword and returns Eliza's response.
 def get_reply(token, text):
     global REPLIES
-    global REPLY_LENGTH
-
+    token_transformation_dict = dict()
     transformation_flag = False
     decoposition_rule = list(chatbot_rules.get(token).keys())
 
@@ -76,27 +67,34 @@ def get_reply(token, text):
     if chatbot_rules.get(token).get("TRANSFORMATION") not in [None, 'null']:
         transformation_flag = True
 
-    words = text.split()
-
+    words = word_tokenize(text)
     for i in range(0, len(words)):
         if words[i] == token and transformation_flag:
             words[i] = chatbot_rules.get(token).get("TRANSFORMATION")
             continue
-        if token in ["I", "YOU"] and words[i] in ["I", "YOU"]:
+        if token == "I" and words[i] == "YOU":
+            words[i] = pronoun_transform(words[i])
+            continue
+        if token == "YOU" and words[i] == "I":
+            words[i] = pronoun_transform(words[i])
             continue
         if words[i] != token:
             pronoun = pronoun_transform(words[i])
 
             if pronoun is not None:
                 words[i] = pronoun
+            else:
+                transformed_token = token_transform(words[i])
+                if transformed_token is not None:
+                    token_transformation_dict[transformed_token] = words[i]
+                    words[i] = transformed_token
     text = " ".join(words)
 
     for rule in decoposition_rule:
 
-        if rule == 'RANK':
+        if rule == 'RANK' or rule == 'TRANSFORMATION':
             continue
-        if rule == 'TRANSFORMATION':
-            continue
+
         rule_t = rule.split()
         pattern = "(" + rule_t[0] + ")"
         for i in range(0, len(rule_t) - 1):
@@ -106,7 +104,7 @@ def get_reply(token, text):
                 pattern = pattern + "(" + rule_t[i + 1] + ")"
 
         pattern = pattern.replace('0', r'[a-zA-Z0-9 _]*').strip()
-
+        # print(pattern, text)
         match = re.match(pattern, " " + text.upper() + " ")
 
         if match is None:
@@ -114,7 +112,7 @@ def get_reply(token, text):
         reply = chatbot_rules.get(token).get(rule)[0]
 
         if "=" in reply:
-            reply = get_reply(reply.replace("=", ""), text)
+            reply = get_reply(reply.replace("=", "").strip(), text)
             return reply
 
         if reply == 'NEWKEY':
@@ -125,12 +123,16 @@ def get_reply(token, text):
             random.shuffle(chatbot_rules.get(token).get(rule))
             for word in reply.split():
                 if word.strip().isnumeric():
-                    reply = reply.replace(word.strip(), match[int(word) - 1])
+                    replacement = match[int(word) - 1]
+                    for key in token_transformation_dict.keys():
+                        replacement = replacement.replace(key, token_transformation_dict[key])
+                    reply = reply.replace(word.strip(), replacement)
 
-            if len(rule) > REPLY_LENGTH:
-                REPLIES = [reply] + REPLIES
-            else:
+            if rule == '0':
                 REPLIES.append(reply)
+            else:
+                REPLIES = [reply]+REPLIES
+                return "DONT_SEARCH_FURTHER"
             return reply
         else:
             reply = "MATCH_NOT_FOUND"
@@ -142,31 +144,35 @@ def pronoun_transform(token):
     pronoun_dict["I"] = "YOU"
     pronoun_dict["YOUR"] = "MY"
     pronoun_dict["MY"] = "YOUR"
-    pronoun_dict["YOU"] = "I"
+    pronoun_dict["YOU"] = "ME"
     pronoun_dict["ME"] = "YOU"
     pronoun_dict["AM"] = "ARE"
-    pronoun_dict["MOTHER"] = "FAMILY"
-    pronoun_dict["FATHER"] = "FAMILY"
-    pronoun_dict["MOM"] = "FAMILY"
-    pronoun_dict["DAD"] = "FAMILY"
-    pronoun_dict["WIFE"] = "FAMILY"
-    pronoun_dict["BROTHER"] = "FAMILY"
-    pronoun_dict["CHILDREN"] = "FAMILY"
-    pronoun_dict["SISTER"] = "FAMILY"
-    pronoun_dict["FEEL"] = "BELIEF"
-    pronoun_dict["THINK"] = "BELIEF"
-    pronoun_dict["BELIEVE"] = "BELIEF"
-    pronoun_dict["CANT"] = "CAN'T"
-    pronoun_dict["WONT"] = "WON'T"
-    pronoun_dict["DONT"] = "DON'T"
-    pronoun_dict["DREAMED"] = "DREAMT"
-    pronoun_dict["DREAMS"] = "DREAMT"
 
     # Make a list of it
     if pronoun_dict.get(token) is not None:
         return pronoun_dict.get(token)
     return None
 
+def token_transform(token):
+    token_dict = dict()
+    token_dict["MOTHER"] = "FAMILY"
+    token_dict["FATHER"] = "FAMILY"
+    token_dict["MOM"] = "FAMILY"
+    token_dict["DAD"] = "FAMILY"
+    token_dict["WIFE"] = "FAMILY"
+    token_dict["BROTHER"] = "FAMILY"
+    token_dict["CHILDREN"] = "FAMILY"
+    token_dict["SISTER"] = "FAMILY"
+    token_dict["FEEL"] = "BELIEF"
+    token_dict["THINK"] = "BELIEF"
+    token_dict["BELIEVE"] = "BELIEF"
+    token_dict["DREAMED"] = "DREAMT"
+    token_dict["DREAMS"] = "DREAMT"
+
+    # Make a list of it
+    if token_dict.get(token) is not None:
+        return token_dict.get(token)
+    return None
 
 # In[9]: Formulating Eliza's greeting, Name tag extraction using POS tagging from NLTK from user input
 greetings = ["Hello I am Eliza, what is your name?", "Hey I am Eliza, what is your name?",
@@ -185,7 +191,7 @@ stopwords = [clean_text(word) for word in stopwords]
 stopwords.extend(newStopWords)
 if name != "":
     name = clean_text(name)
-    tokens = nltk.word_tokenize(name)
+    tokens = word_tokenize(name)
     tokens_tagged = nltk.pos_tag(tokens)
     tokens_tagged = [token for token in tokens_tagged if token[0].lower() not in set(stopwords)]
 
@@ -211,50 +217,46 @@ while True:
 
     try:
         REPLIES = []
-        REPLY_LENGTH = 0
-        inp = str(input(f"{name}:"))
-        if inp.upper() == "QUIT":
-            print("thank you for talking to me. If you need me anytime I am just a click away!!\nbye")
-            break
-
-        inp = clean_text(inp)
-        inp_tok = inp.strip().upper().split()
-        key_stack = []
+        KEY_STACK = []
         RANK = 0
 
-        dic_tok_trans = {}
-        for token in inp_tok:
+        user_input = str(input(f"{name}:"))
+
+        if user_input.lower() == "quit":
+            print("thank you for talking to me. If you need me anytime I am just a click away!!\nbye")
+            break
+        # Replies are based on first sentence only if a user enters more than 1 sentence.
+        user_input = sent_tokenize(user_input)[0]
+        user_input = clean_text(user_input)
+        input_tokens = word_tokenize(user_input.strip().upper())
+
+        transformation_dict = dict()
+        for token in input_tokens:
             token_rank = get_rank(token)
-            if token_rank == "TOKEN_FOUND":
-                continue
-            elif token_rank == "TOKEN_NOT_FOUND":
+            if token_rank == "TOKEN_NOT_FOUND" and chatbot_rules['TRANSFORMATION'].get(token) is not None:
+                token_transformation = chatbot_rules['TRANSFORMATION'].get(token)
+                transformation_dict[token_transformation] = token
+                get_rank(token_transformation)
 
-                if chatbot_rules['TRANSFORMATION'].get(token) is not None:
-                    token1 = chatbot_rules['TRANSFORMATION'].get(token)
-                    dic_tok_trans[token1] = token
-                    get_rank(token1)
+        # If there are no keywords present in user input
+        if len(KEY_STACK) == 0:
+            reply = get_reply("NONE", user_input)
 
-        if len(key_stack) == 0:
-            reply = get_reply("NONE", inp)
-
-        for key in key_stack:
-            if dic_tok_trans.get(key) is not None:
-                inp_text = inp.replace(dic_tok_trans.get(key), key)
+        for key in KEY_STACK:
+            if transformation_dict.get(key) is not None:
+                inp_text = user_input.replace(transformation_dict.get(key), key)
             else:
-                inp_text = inp
+                inp_text = user_input
 
             reply = get_reply(key, inp_text.upper())
-            if reply in ["DECOMPOSITION_RULE_NOT_FOUND", "MATCH_NOT_FOUND",
-                         "NEXT_KEY"] or reply is None or reply.upper() == 'NONE':
-                continue
-            else:
+            if reply == "DONT_SEARCH_FURTHER":
                 break
-            REPLIES.append({key: reply})
+            if reply in ["DECOMPOSITION_RULE_NOT_FOUND", "MATCH_NOT_FOUND",
+                         "NEXT_KEY"] or reply is None or reply.lower() == 'none':
+                reply = get_reply("NONE", user_input)
     except:
-        reply = get_reply("NONE", inp)
-
-    if reply in ["DECOMPOSITION_RULE_NOT_FOUND", "MATCH_NOT_FOUND",
-                 "NEXT_KEY"] or reply is None or reply.upper() == 'NONE':
-        reply = get_reply("NONE", inp)
-
+        reply = "Uh-oh!, somethings broken. Please send feedback to the admin regarding this to make me better."
+    print(KEY_STACK )
+    print(REPLIES)
+    reply = REPLIES[0]
     print(f"Eliza: {reply}")
